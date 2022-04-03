159a1a76c847fcb9e88793e07ff13dd1c956050e
AzmiHabib <azmihabib0323@gmail.com>
2022-04-03 22:12:49 +0600

    commit after routing all header option

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header/Header";
-
+import Home from './Components/Home/Home';
+import Review from './Components/Review/Review';
+import About from './Components/About/About';
+import DashBoard from './Components/Dashboard/Dashboard';
+import Blogs from './Components/Blogs/Blogs';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
+      <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/review' element={<Review></Review>}></Route>
+      <Route path='/about' element={<About></About>}></Route>
+      <Route path='/dashBoard' element={<DashBoard></DashBoard>}></Route>
+      <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+      </Routes>
     </div>
   );
 }
