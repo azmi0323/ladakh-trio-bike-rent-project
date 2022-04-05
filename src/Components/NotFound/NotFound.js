5311042296726e12fc6254f9545f88fe500c46c0
AzmiHabib <azmihabib0323@gmail.com>
2022-04-05 14:57:10 +0600

    final commit

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import './NotFound.css'
+import React from "react";
+import "./NotFound.css";
+import notFoundPic from "../../Assets/404.png";
 const NotFound = () => {
-    return (
-        <div>
-            <h1>404 Not Found</h1>
-        </div>
-    );
+  return (
+    <div>
+      <div className="d-flex align-content-center justify-content-center">
+        <img src={notFoundPic} alt="" />
+      </div>
+      <div className="d-flex align-content-center justify-content-center">
+        <h1 className="text-danger fw-bold mb-5">Page Not Found</h1>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
